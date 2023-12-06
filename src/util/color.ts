a701c2606ddd965049e1baebef38f3a98e050da3
HWANBINYOO <dngh0825@naver.com>
2023-12-07 08:10:43 +0900

    Add : Color

---
+++ src/util/color.ts
@@ -0,0 +1,11 @@
+export const theme = {
+  mainColor: "#9288f8",
+  red: "#FF7D7D",
+  purple: "#55155d",
+  pink: "#982E81",
+  gray: "#818198",
+  orange: "#EE786D",
+  blue: "#5966E9",
+  yellow: "#eff4af",
+  green: "#7eb293",
+};
