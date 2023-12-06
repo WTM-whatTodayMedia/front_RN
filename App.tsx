60a9a016e58c59bdafcbe6bef8631ce5dcc8ffc5
HWANBINYOO <dngh0825@naver.com>
2023-12-07 08:08:21 +0900

    Fix : App.tsx

--- App.tsx
+++ App.tsx
@@ -1,5 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+// const Stack = createStackNavigator();
+// const Tab = createBottomTabNavigator();
+// const queryClient = new QueryClient();
 
 export default function App() {
   return (
@@ -13,8 +21,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
