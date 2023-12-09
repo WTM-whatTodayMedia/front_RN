16a71f3a3d3457f0f629d00ef6b3955a3dff1418
HWANBINYOO <dngh0825@naver.com>
2023-12-09 14:21:42 +0900

    Add : HomeScreen filter 기능구현

--- src/screen/HomeScreen/HomeScreen.tsx
+++ src/screen/HomeScreen/HomeScreen.tsx
@@ -1,18 +1,28 @@
 import { Text, View, ScrollView } from "react-native";
 import { S } from "./Style";
 import { useQuery } from "react-query";
-import { fetchList } from "../../api/api";
+import { fetchList, getFilterList } from "../../api/api";
 import { listProps } from "../../types/list";
 import ListBox from "../../components/ListBox";
 import Header from "../../components/common/Header/Header";
+import { useEffect, useState } from "react";
 
 function HomeScreen() {
   const { data } = useQuery("posts", fetchList);
-  const list: listProps[] = data?.results;
+  const [list, setList] = useState<listProps[]>(data);
+
+  const onHandleCategoryClick = async (category: string) => {
+    if (category === "영화" || category === "드라마") {
+      const filterList: listProps[] = await getFilterList(category);
+      setList(filterList);
+    } else {
+      setList(data);
+    }
+  };
 
   return (
     <View style={S.Wrapper}>
-      <Header />
+      <Header onCategoryClick={onHandleCategoryClick} />
       <ScrollView style={S.List} horizontal={false}>
         <View style={S.StylegridView}>
           {list && list.length > 0 ? (
