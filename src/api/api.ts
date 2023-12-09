0b09b7780b8bfa4353fedf6da5a6c9e3f7847a36
HWANBINYOO <dngh0825@naver.com>
2023-12-09 14:20:28 +0900

    Add : getFilterList api

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,10 +1,22 @@
-import { useMutation } from "react-query";
 import apiClient from "../util/lib/apiClient";
 
 export const fetchList = async () => {
+  let objectArray: object[] = [];
+  objectArray.push({
+    property: "Tag",
+    select: {
+      equals: "",
+    },
+  });
   try {
-    const { data } = await apiClient.post("");
-    return data;
+    const { data } = await apiClient.post("", {
+      filter: {
+        and: objectArray,
+      },
+    });
+    const list = data.results;
+
+    return list;
   } catch (e) {
     console.log(e);
     return {};
@@ -14,4 +26,26 @@ export const fetchList = async () => {
 export const fetchListItem = async (id: number) => {
   const { data } = await apiClient.get(`/questions/${id}`);
   return data;
+};
+
+export const getFilterList = async (names: string) => {
+  let objectArray: object[] = [];
+  objectArray.push({
+    property: "Tag",
+    select: {
+      equals: names,
+    },
+  });
+  try {
+    const { data } = await apiClient.post("", {
+      filter: {
+        and: objectArray,
+      },
+    });
+    const list = data.results;
+    return list;
+  } catch (e) {
+    console.log(e);
+    return {};
+  }
 };
